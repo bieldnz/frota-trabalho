5dce3c41d8a34d2d01445918e855ee46adc87b9e
Ruan Freires <rgnf2015@gmail.com>
2025-10-18 10:15:57 -0300

    criação da caixa

---
+++ src/main/java/com/example/frota/caixa/CaixaService.java
@@ -0,0 +1,31 @@
+package com.example.frota.caixa;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class CaixaService {
+    private final CaixaRepository caixaRepository;
+    public CaixaService(CaixaRepository caixaRepository) {
+        this.caixaRepository = caixaRepository;
+    }
+    // Métodos de serviço para gerenciar caix
+    public List<Caixa> procurarTodos() {
+        return caixaRepository.findAll();
+    }
+    public Optional<Caixa> procurarPorId(Long id) {
+        return caixaRepository.findById(id);
+    }
+    public Caixa salvar(Caixa novaCaixa) {
+        return caixaRepository.save(novaCaixa);
+    }
+    public void deletar(Long id) {
+        caixaRepository.deleteById(id);
+    }
+    public Caixa atualizar(Caixa caixaAtualizada) {
+        return caixaRepository.save(caixaAtualizada);
+    }
+
+}
---
+++ src/main/java/com/example/frota/caixa/caixa.java
@@ -0,0 +1,43 @@
+package com.example.frota.caixa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "caixa")
+@Getter
+@Setter
+@EqualsAndHashCode(of ="id")
+public class Caixa {
+    @Id
+    @GeneratedValue (strategy =GenerationType.IDENTITY)
+    @Column(name = "caixa_id")
+    private Long id;
+    private String material;
+    private double capacidadeKg;
+    private boolean disponivel;
+    private double altura;
+    private double largura;
+    private double profundidade;
+
+    // Construtores, getters e setters
+    public Caixa() {
+    }
+    public Caixa(Long id, String material, double capacidadeKg, boolean disponivel, double altura, double largura, double profundidade ) {
+        this.id = id;
+        this.material = material;
+        this.capacidadeKg = capacidadeKg;
+        this.disponivel = disponivel;
+        this.altura = altura;
+        this.largura = largura;
+        this.profundidade = profundidade;
+    }
+    
+}
